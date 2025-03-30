5d6c4c77e40f46634830330af08e1425dfacad9b
shubhamkunwar10 <fonders@metakul.com>
2025-03-30 23:47:47 +0530

    Update icon

--- src/Layout/navigation.tsx
+++ src/Layout/navigation.tsx
@@ -3,17 +3,19 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
 // Header Links
 export const HEADER_LINKS = [
-  { to: '/', label: 'Dashboard' },
-  { to: '/about-us', label: 'About',icon:<AnalyticsIcon/> },
-  { to: '/settings', label: 'Settings',icon:<SettingsIcon/> },
+  { to: '/', label: 'Dashboard',icon:<HomeIcon sx={{color:"white"}}/> },
+  { to: '/about-us', label: 'About',icon:<InfoIcon sx={{color:"white"}}/> },
+  { to: '/settings', label: 'Settings',icon:<SettingsIcon sx={{color:"white"}}/> },
 ];
 
 // Sidebar Items
 export const sidebarItems = [
-    { to: '/dashboard', label: 'Dashboard',icon: <DashboardIcon/>},
-    { to: '/orders', label: 'Orders',icon:<LocalMallIcon/> },
-    { to: '/analytics', label: 'Analytics',icon :<AnalyticsIcon/>},
+    { to: '/dashboard', label: 'Dashboard',icon: <DashboardIcon sx={{color:"white"}}/>},
+    { to: '/orders', label: 'Orders',icon:<LocalMallIcon sx={{color:"white"}}/> },
+    { to: '/analytics', label: 'Analytics',icon :<AnalyticsIcon sx={{color:"white"}}/>},
     { to: '/layers', label: 'Layers' }
   ];
