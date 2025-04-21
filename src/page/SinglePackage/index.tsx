833a5501c1b17a89f12817350ea94f0d2ea1f1b7
shubhamkunwar10 <fonders@metakul.com>
2025-04-22 01:43:28 +0530

    Upate mediabackgorund

--- src/page/SinglePackage/index.tsx
+++ src/page/SinglePackage/index.tsx
@@ -141,15 +141,9 @@ console.log(videos)
     <div className="relative z-10">
       <div className="absolute inset-0 overflow-hidden">
 <div className="absolute inset-0 overflow-hidden">
-  {videos.length > 0 ? (
+  {videos.length > 0 &&
     <MediaBackground video={{ base64Data: videos[0] }} /> 
-  ) : (
-    <img
-      src={`data:image/jpeg;base64,${image}`}
-      alt={title}
-      className="w-full h-full object-cover"
-    />
-  )}
+}
 </div>
       </div>
       <div className="backdrop-blur-[4px] bg-black/40 min-h-screen pt-24">
